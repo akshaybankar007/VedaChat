e9bf930396fa49539d98602e108a0ce8ef25664d
Akshay Yogeshwar Bankar <akshaybankar076@gmail.com>
2026-07-12 09:52:17 +0530

    feat: create auth context to prevent anonymous ghosts from using the app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import { SocketProvider } from './context/SocketContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </AuthProvider>
   </StrictMode>,
 )
