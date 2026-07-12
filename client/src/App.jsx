535d9dcef6d1cc9fb46db86ec2f489fbf4fcf15e
Akshay Yogeshwar Bankar <akshaybankar076@gmail.com>
2026-07-12 09:57:09 +0530

    feat: implement registration flow so anyone can create account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Chat from "./pages/Chat";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
         <Route path="/" element={user ? <Chat /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
