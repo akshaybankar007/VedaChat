870e1e8c658066464219ab40f39e47b94062664f
Akshay Yogeshwar Bankar <akshaybankar076@gmail.com>
2026-07-12 17:23:22 +0530

    feat: attach last message preview to users list for sidebar UI

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,13 +1,36 @@
 import User from "../models/User.js";
+import Message from "../models/Message.js";
+import mongoose from "mongoose";
 
 export const getUsers = async (req, res) => {
     try {
-        // Fetch everyone except the person asking, because they already know they exist. I even don't know whether someone will see this. If they see this comment, they probably will laugh. Haha!!!!
-        const users = await User.find({ _id: { $ne: req.user.id } })
+        const currentUserId = req.user.id;
+        
+        // Fetch users except current
+        const users = await User.find({ _id: { $ne: currentUserId } })
             .select("username isOnline lastSeen")
-            .sort({ isOnline: -1, username: 1 });
+            .sort({ isOnline: -1, username: 1 })
+            .lean();
             
-        res.json({ success: true, users });
+        // Append last message for sidebar preview
+        const usersWithMessages = await Promise.all(users.map(async (user) => {
+            const lastMsg = await Message.findOne({
+                $or: [
+                    { sender: currentUserId, receiver: user._id },
+                    { sender: user._id, receiver: currentUserId }
+                ]
+            })
+            .sort({ createdAt: -1 })
+            .select("text createdAt");
+
+            return {
+                ...user,
+                lastMessage: lastMsg ? lastMsg.text : null,
+                lastMessageTime: lastMsg ? lastMsg.createdAt : null
+            };
+        }));
+            
+        res.json({ success: true, users: usersWithMessages });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
