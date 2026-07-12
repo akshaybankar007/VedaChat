e7bf764961ef592e9efd7a2b7cccfc0b40a80d5c
Akshay Yogeshwar Bankar <akshaybankar076@gmail.com>
2026-07-12 17:22:27 +0530

    feat: update message route to accept target user ID parameter

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -4,6 +4,6 @@ import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", protect, getMessages);
+router.get("/:userId", protect, getMessages);
 
 export default router;
