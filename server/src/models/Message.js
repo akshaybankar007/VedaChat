7cab44ab68d75e69f51bc240b7e07614042a5f47
Akshay Yogeshwar Bankar <akshaybankar076@gmail.com>
2026-07-12 17:21:52 +0530

    feat: add receiver field to message model for 1-to-1 conversation isolation

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -7,6 +7,11 @@ const messageSchema = new mongoose.Schema(
             ref: "User", 
             required: true 
         },
+        receiver: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User", 
+            required: true 
+        },
         text: { 
             type: String, 
             required: true 
